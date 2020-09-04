0636253b181aba5c811f88fcbffbc952fd647c73
prodentechnologiescbe <68459621+prodentechnologiescbe@users.noreply.github.com>
2020-09-04 19:22:31 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,25 +1,10 @@
-import pytest
-# def test_file1_method2():
-# 	x=5
-# 	y=6
-# 	assert x+1 == y,"test failed" 
-# print('Test Hello --> 1.3 Bharath')
-
-from flask import jsonify
-from flask import flash, request
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/data')
-def users():
-	try:
-		rows = [(1,'vignesh'),(2,'test')]
-		resp = jsonify(rows)
-		resp.status_code = 200
-		return resp
-	except Exception as e:
-		print(e)
-
-if __name__ == "__main__":
-    app.run()
+ from flask import Flask 
+app = Flask(__name__) 
+  
+@app.route('/') 
+def hello(): 
+    return "welcome to the flask tutorials"
+  
+  
+if __name__ == "__main__": 
+    app.run(host ='0.0.0.0', port = 5001, debug = True)
