506df8eb74ceb8c4183243dab23a4a997ac2d60c
prodentechnologiescbe <68459621+prodentechnologiescbe@users.noreply.github.com>
2020-09-04 15:59:47 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,25 @@
 import pytest
-def test_file1_method2():
-	x=5
-	y=6
-	assert x+1 == y,"test failed" 
-print('Test Hello --> 1.3 Bharath')
+# def test_file1_method2():
+# 	x=5
+# 	y=6
+# 	assert x+1 == y,"test failed" 
+# print('Test Hello --> 1.3 Bharath')
+
+from flask import jsonify
+from flask import flash, request
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/data')
+def users():
+	try:
+		rows = [(1,'vignesh'),(2,'test')]
+		resp = jsonify(rows)
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+
+if __name__ == "__main__":
+    app.run()
